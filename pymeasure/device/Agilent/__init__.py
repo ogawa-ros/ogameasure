73c874c16d52ebde1a9501316b437d63d4187bd0
ars096 <ars096@gmail.com>
2014-12-22 02:44:22 +0900

    create E4418, and implemented error class

--- pymeasure/device/Agilent/__init__.py
+++ pymeasure/device/Agilent/__init__.py
@@ -1,5 +1,12 @@
 #! /usr/bin/env python
 
+
+# Power Meter
+# -----------
+from E4418 import EPM441A
+
+# Attenuator/Switch Driver
+# ------------------------
 from agilent_11713 import agilent_11713B
 from agilent_11713 import agilent_11713C
 
