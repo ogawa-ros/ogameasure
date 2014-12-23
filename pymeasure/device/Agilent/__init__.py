c3c07c74ca3048f4a4b976c6b0fb1b575dc769e6
ars096 <ars096@gmail.com>
2014-12-23 15:48:48 +0900

    closed #16, created N9343

--- pymeasure/device/Agilent/__init__.py
+++ pymeasure/device/Agilent/__init__.py
@@ -8,6 +8,12 @@ from E4418 import EPM442A
 from E4418 import E4418B
 from E4418 import E4419B
 
+# Spectrum Analyzer
+# -----------------
+from N9342 import N9342C
+from N9342 import N9343C
+from N9342 import N9344C
+
 # Attenuator/Switch Driver
 # ------------------------
 from agilent_11713 import agilent_11713B
