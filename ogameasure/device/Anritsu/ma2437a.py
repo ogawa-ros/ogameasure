afcffd70ff6720f005eecf2858f1a29bdfdc05ee
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-07-02 18:50:40 +0900

    add

--- ogameasure/device/Anritsu/ma2437a.py
+++ ogameasure/device/Anritsu/ma2437a.py
@@ -5,12 +5,6 @@ from ..SCPI import scpi
 
 class ma2437a(scpi.scpi_family):
 
-    def __init__(self):
-        ch = 1
-        for i in range(ch):
-            self.com.send(b'CHUNIT %d, DBM' %(i))
-            self.com.send(b'CHRES %d, %d' %(i, resolution))
-
 
     def measure(self, ch=1, resolution=3):
         '''
