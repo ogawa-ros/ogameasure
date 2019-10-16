f2e89bb197a9650afdba209234db31040acf88de
kotakayoshi <tatuyuki.takashima@gmail.com>
2019-10-16 22:26:19 +0900

    tpg_lan

--- ogameasure/device/Pfeiffer/tpg261_lan.py
+++ ogameasure/device/Pfeiffer/tpg261_lan.py
@@ -15,8 +15,11 @@ class tpg261_lan():
         self.com.send(b"\x05")
         time.sleep(0.3)
         self.raw_p = self.com.recv().decode('utf-8').strip().split(',')
-        pressure = numpy.float64(self.raw_p[1])
-        return pressure
+        if type(self.raw_p[2])==str: 
+            pressure = numpy.float64(self.raw_p[2])
+            return pressure
+        else:
+            pass
 
     def tpg_status(self):
         self.com.send(b"PR1 \r\n")
