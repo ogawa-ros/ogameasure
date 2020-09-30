549fe1e8d0612ef7efdc34ce15a1542f0d4d3b39
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-09-30 17:08:31 +0900

    update tr_73u

--- ogameasure/device/TandD/tr_73u.py
+++ ogameasure/device/TandD/tr_73u.py
@@ -1,7 +1,8 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 import time
 import serial
+import struct
 
 class tr_73u(object):
 
@@ -14,12 +15,22 @@ class tr_73u(object):
         ser = serial.Serial(port=port, baudrate=19200, timeout=5, stopbits=1)
         return ser
 
+    def start(self):
+        self.write(b'0x00')
+        return
+
     def write(self, cmd):
         self.ser.write(cmd)
         return
 
-    def read(self, byte=100):
-        ret = self.ser.read(byte)
+    def output_current_data(self):
+        data = self.query(cmd=b'\x01\x33\x00\x04\x00\x00\x00\x00\x00\x38\x00', byte=26)
+        #data = 26byte, format=B
+        data = struct.unpack('26B', data)
+        ret = {}
+        ret['temp'] = (data[6]*16**2+data[5]-1000)/10
+        ret['humid'] = (data[8]*16**2+data[7]-1000)/10
+        ret['press'] = (data[10]*16**2+data[9])/10
         return ret
 
     def query(self, cmd, byte, wt=0.01):
@@ -28,15 +39,6 @@ class tr_73u(object):
         ret = self.read(byte)
         return ret
 
-    def start(self):
-        self.write(b'0x00')
-        return
-
-    def output_current_data(self):
-        data = self.query(cmd=b'\x01\x33\x00\x04\x00\x00\x00\x00\x00\x38\x00', byte=26)
-        return data
-
-    #data = 26byte, format=B
-    #temp = (data[6]*16**2+data[5]-1000)/10
-    #humid = (data[8]*16**2+data[7]-1000)/10
-    #press = (data[10]*16**2+data[9])/10
+    def read(self, byte=100):
+        ret = self.ser.read(byte)
+        return ret
