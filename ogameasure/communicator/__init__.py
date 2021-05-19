e594e379449700a0fd393800103649b9f118461f
Nishimura, Atsushi <ars096@gmail.com>
2021-05-19 16:16:49 +0900

    add serial communicator

--- ogameasure/communicator/__init__.py
+++ ogameasure/communicator/__init__.py
@@ -1,3 +1,4 @@
 from .ethernet import ethernet
 from .gpib_prologix import gpib_prologix
+from .serial import serial
 from .usb import usb
---
+++ ogameasure/communicator/serial.py
@@ -0,0 +1,85 @@
+import serial as pyserial
+from . import communicator
+
+class serial(communicator.communicator):
+    method = 'serial'
+
+    port = None
+    baudrate = 9600
+    bytesize = pyserial.EIGHTBITS
+    parity = pyserial.PARITY_NONE
+    stopbits = pyserial.STOPBITS_ONE
+    timeout = None
+    xonxoff = False
+    rtscts = False
+    write_timeout = None
+    dsrdtr = False
+    inter_byte_timeout = None
+
+    def __init__(self, port,
+                 baudrate = 9600,
+                 bytesize = pyserial.EIGHTBITS,
+                 parity = pyserial.PARITY_NONE,
+                 stopbits = pyserial.STOPBITS_ONE,
+                 timeout = None,
+                 xonxoff = False,
+                 rtscts = False,
+                 write_timeout = None,
+                 dsrdtr = False,
+                 inter_byte_timeout = None):
+        self.port = port
+        self.baudrate = baudrate
+        self.bytesize = bytesize
+        self.parity = parity
+        self.stopbits = stopbits
+        self.timeout = timeout
+        self.xonxoff = xonxoff
+        self.rtscts = rtscts
+        self.write_timeout = write_timeout
+        self.dsrdtr = dsrdtr
+        self.inter_byte_timeout = inter_byte_timeout
+        pass
+
+    def open(self):
+        if self.connection == False:
+            self.ser = pyserial.Serial(
+                port = self.port,
+                baudrate = self.baudrate,
+                bytesize = self.bytesize,
+                parity = self.parity,
+                stopbits = self.stopbits,
+                timeout = self.timeout,
+                xonxoff = self.xonxoff,
+                rtscts = self.rtscts,
+                write_timeout = self.write_timeout,
+                dsrdtr = self.dsrdtr,
+                inter_byte_timeout = self.inter_byte_timeout,
+            )
+
+            self.connection = True
+            pass
+        return
+
+    def close(self):
+        self.ser.close()
+        del(self.ser)
+        self.connection = False
+        return
+
+    def send(self, msg):
+        self.ser.write((msg+self.terminator).encode())
+        return
+
+    def recv(self, byte=1024):
+        ret = self.ser.read(byte)
+        try:
+            ret = ret.decode('ascii')
+            pass
+        return ret
+
+    def readline(self):
+        ret = self.ser.readline()
+        try:
+            ret = ret.decode('ascii')
+            pass
+        return ret
