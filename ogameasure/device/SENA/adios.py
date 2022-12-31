26489859af8b193862f1808965e7d7d69e14810e
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-12-31 15:28:39 +0900

    #27 Add error handler

--- ogameasure/device/SENA/adios.py
+++ ogameasure/device/SENA/adios.py
@@ -1,5 +1,6 @@
 import time
-from ...communicator import communicator
+from serial import SerialTimeoutException
+from socket import TimeoutError
 
 class adios(object):
 
@@ -90,18 +91,21 @@ class adios(object):
                 else: pass
                 #print temp
                 break
-            except (communicator.CommunicatorTimeout, UnicodeDecodeError):
+            except (SerialTimeoutException, TimeoutError, UnicodeDecodeError):
                 print('(error) att1')
                 continue
             continue
         time.sleep(0.1)
 
-        do1 = int(temp[-16])*1
-        do2 = int(temp[-15])*2
-        do3 = int(temp[-14])*4
-        do4 = int(temp[-13])*8
-        do5 = int(temp[-11])*16
-        att1 = do1+do2+do3+do4+do5
+        try:
+            do1 = int(temp[-16])*1
+            do2 = int(temp[-15])*2
+            do3 = int(temp[-14])*4
+            do4 = int(temp[-13])*8
+            do5 = int(temp[-11])*16
+            att1 = do1+do2+do3+do4+do5
+        except ValueError:
+            att1 = -1
         return att1
 
     def get_att2(self):
@@ -112,16 +116,19 @@ class adios(object):
                 if temp.find(':')==-1: continue
                 else: pass
                 break
-            except (communicator.CommunicatorTimeout, UnicodeDecodeError):
+            except (SerialTimeoutException, TimeoutError, UnicodeDecodeError):
                 print('(error) att2')
                 continue
             continue
         time.sleep(0.1)
 
-        do6 = int(temp[-10])*1
-        do7 = int(temp[-9])*2
-        do8 = int(temp[-8])*4
-        do9 = int(temp[-6])*8
-        do10 = int(temp[-5])*16
-        att2 = do6+do7+do8+do9+do10
+        try:
+            do6 = int(temp[-10])*1
+            do7 = int(temp[-9])*2
+            do8 = int(temp[-8])*4
+            do9 = int(temp[-6])*8
+            do10 = int(temp[-5])*16
+            att2 = do6+do7+do8+do9+do10
+        except ValueError:
+            att2 = -1
         return att2
