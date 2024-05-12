ac0d90d629aedc672e2fa6f46e71c8e7c638efeb
Tatsumi ISHIKAWA <112744259+TatsumiISHIKAWA@users.noreply.github.com>
2024-05-12 23:29:22 +0900

    #46 add code and black

--- ogameasure/device/Anritsu/__init__.py
+++ ogameasure/device/Anritsu/__init__.py
@@ -2,3 +2,4 @@
 # -----------
 from .ml2437a import ml2437a
 from .ma24126a import ma24126a
+from .mg3692c import mg3692c
---
+++ ogameasure/device/Anritsu/mg3692c.py
@@ -0,0 +1,68 @@
+import time
+from ..SCPI import scpi
+
+# main class
+# ==========
+
+delay_time = 0.1
+
+
+class InvalidRangeError(Exception):
+    pass
+
+
+class mg3692c(scpi.scpi_family):
+    manufacturer = "Anritsu"
+    product_name = "MG3692C"
+    classification = "Signal Generator"
+
+    _scpi_enable = "*IDN? *RCL *RST"  # TODO: check the available command
+
+    freq_range_ghz = (2, 20)
+    power_default_dbm = 0
+    power_range_dbm = (-20, 30)
+
+    def set_freq(self, freq, unit="GHz"):
+        self.com.send("FREQ:CW %.10f %s" % (freq, unit))
+        self.com.close()
+        return
+
+    def get_freq(self):
+        self.com.send("FREQ:CW?")
+        ret = self.com.readline()
+        freq = float(ret) / 1e9
+
+        return freq
+
+    def set_power(self, power=-20.0):
+        if -20.0 <= power <= 30.0:
+            self.com.send("POW %f dBm" % (power))
+        else:
+            msg = "Power range is -20.0[dBm] -- 30.0[dBm],"
+            msg += " while {}[dBm] is given.".format(power)
+            raise InvalidRangeError(msg)
+        return
+
+    def get_power(self):
+        self.com.send("POW?")
+        ret = self.com.readline()
+        power = float(ret)
+
+        return power
+
+    def set_onoff(self, onoff=0):
+        if onoff == 1:
+            self.com.send("OUTP ON")
+        else:
+            self.com.send("OUTP OFF")
+        return
+
+    def get_onoff(self):
+        self.com.send("OUTP?")
+        ret = self.com.readline()
+        ret = int(ret)
+
+        return ret
+
+    def close(self):
+        self.com.close()
