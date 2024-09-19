1c45020d56145a6e2388602454140b633601ade6
Tatsumi ISHIKAWA <112744259+TatsumiISHIKAWA@users.noreply.github.com>
2024-09-19 14:49:28 +0900

    #64 add alarm func

--- ogameasure/device/OrientalMotor/azd_ad.py
+++ ogameasure/device/OrientalMotor/azd_ad.py
@@ -6,7 +6,13 @@ class azd_ad(device.device):
     manufacturer = "OrientalMotor"
     product_name = "AZD-AD"
     classification = "Motor Controller"
-    # TODO: Add Error code query. Plz check in COSMOTECH sp100.py.
+
+    def initialize(self):
+        self.simulator_onoff(False)
+        self.zero_return()
+        self.alarm_reset()
+        self.alarm_clear()
+        return
 
     def zero_return(self):
         """
@@ -65,6 +71,54 @@ class azd_ad(device.device):
         msg = self.com.recv().hex()
         position = int(msg[10:14], 16)
         return position
+
+    def alarm_query(self):
+        query_command = create_query("010300800002")
+        self.com.send_raw(query_command)
+        time.sleep(0.05)
+        msg = self.com.recv().hex()
+        alarm_code = int(msg[10:14], 16)
+        return alarm_code
+
+    def alarm_reset(self):
+        reset_command = create_query("011001800002040000")
+        self.com.send_raw(reset_command)
+        time.sleep(0.05)
+        _ = self.com.recv().hex()
+        reset_command = create_query("011001800002040001")
+        self.com.send_raw(reset_command)
+        time.sleep(0.05)
+        _ = self.com.recv().hex()
+        return
+
+    def alarm_clear(self):
+        clear_command = create_query("011001840002040000")
+        self.com.send_raw(clear_command)
+        time.sleep(0.05)
+        _ = self.com.recv().hex()
+        clear_command = create_query("011001840002040001")
+        self.com.send_raw(clear_command)
+        time.sleep(0.05)
+        _ = self.com.recv().hex()
+        return
+
+    def simulator_onoff(self, onoff: bool):
+        if onoff:
+            sim_command = create_query("011003FE0002040001")
+        else:
+            sim_command = create_query("011003FE0002040000")
+        self.com.send_raw(sim_command)
+        time.sleep(0.05)
+        _ = self.com.recv().hex()
+        return
+
+    def simulator_query(self):
+        sim_command = create_query("010303FE0002")
+        self.com.send_raw(sim_command)
+        time.sleep(0.05)
+        msg = self.com.recv().hex()
+        mode = int(msg[10:14], 16)
+        return mode
 
 
 def crc16(command):
